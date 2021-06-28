6d0ff462d6dd07a5d4af5b7d3b62d73ca2cb805b
Reis <jonyreiscardoso@gmail.com>
2021-06-28 15:09:35 -0400

    feat: add Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Title = styled.Text`
+  color: #707070;
+  text-align: center;
+  font-style: italic;
+  font-size: 44px;
+  font-weight: 700;
+  margin: auto;
+`;
+
+const Home = () => {
+  return (
+    <Title>Home</Title>
+  )
+}
+
+export default Home
