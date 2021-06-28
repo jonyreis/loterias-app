4922d4604e0391554f5219fe37eaa905695383eb
Reis <jonyreiscardoso@gmail.com>
2021-06-28 15:06:42 -0400

    feat: add store

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import Routes from './src/routes'
+import store from './src/store'
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar style="auto" />
       <Routes />
-    </>
+    </Provider>
   )
 }
