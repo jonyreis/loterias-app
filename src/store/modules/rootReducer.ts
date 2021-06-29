f828de61d9dae6b7a0fc546226c7fdde34628b2d
Reis <jonyreiscardoso@gmail.com>
2021-06-29 14:52:01 -0400

    feat: add gamesReducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 import authReducer from './auth/reducer'
+import gamesReducer from './games/reducer'
+
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  games: gamesReducer
 })
