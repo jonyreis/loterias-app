911ac567f9c3f0d7cf9cb7d4fc33871bf2431de6
Reis <jonyreiscardoso@gmail.com>
2021-06-29 14:48:02 -0400

    feat: add getGame function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { useDispatch, useSelector, RootStateOrAny } from 'react-redux'
+
+import GamesBtn from '../../components/GamesBtn'
+
+import api from '../../services/api'
+
+
 import styled from 'styled-components/native'
+
 
 const Title = styled.Text`
   color: #707070;
@@ -11,8 +19,56 @@ const Title = styled.Text`
 `;
 
 const Home = () => {
+  const [games, setGames] = React.useState([])
+  const { auth } = useSelector((state: RootStateOrAny) => state)
+  const dispatch = useDispatch()
+
+  React.useEffect(() => {
+    getGame()
+  }, [])
+
+  async function getGame() {
+    await api.get('/games', { 
+      headers: {
+        "Authorization": `Bearer ${auth.token}`
+      }
+    }).then((res) => setGames(res.data)).catch((err) => alert(err.message))
+
+
+    const listGame = games.map((item: {
+      id: number;
+      color: string; 
+      type: string; 
+      description: string;
+      ['max_number']: number;
+      ['min_cart_value']: number;
+      price: number; 
+      range: number; 
+    }) => {
+      return {
+        id: item.id,
+        color: item.color,
+        type: item.type,
+        description: item.description,
+        maxNumber: item['max_number'],
+        minCartValue: item['min_cart_value'],
+        price: item.price,
+        range: item.range
+      }
+    })
+
+    dispatch({
+      type: 'ADD_GAMES',
+      payload: listGame
+    })
+
+  }
+
   return (
-    <Title>Home</Title>
+    <>
+      <GamesBtn />
+      <Title>Home</Title>
+    </>
   )
 }
 
---
+++ src/store/modules/games/types.ts
@@ -0,0 +1,10 @@
+export interface IGames {
+  id: number | null
+  type: string
+  color: string
+  description: string
+  maxNumber: number
+  minCartValue: number
+  price: number
+  range: number
+}
