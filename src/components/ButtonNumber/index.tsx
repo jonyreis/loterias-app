6653fce6a535b6180d06217a3469759de1d1ec20
Reis <jonyreiscardoso@gmail.com>
2021-07-02 15:36:13 -0400

    feat: create ButtonNumber component

---
+++ src/components/ButtonNumber/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ButtonNumberContainer, NumberText } from './styles'
+
+interface IButtonNumberProps {
+  value: number,
+  selected: boolean,
+  selectedGame: any,
+  arraySelectedNumbers?: any,
+  setArraySelectedNumbers?: any
+}
+
+const ButtonNumber = (props: IButtonNumberProps) => {
+  function handleSelectNumber(btnNumber: any) {
+    const num = Number(btnNumber.currentTarget.textContent)
+
+    if (props.arraySelectedNumbers.indexOf(num) === -1 && props.arraySelectedNumbers.length < props.selectedGame.maxNumber) {
+      btnNumber.currentTarget.style.background = '#27c383'
+      props.arraySelectedNumbers.push(num)
+    } else if (props.arraySelectedNumbers.indexOf(num) !== -1) {
+      btnNumber.currentTarget.style.background = '#adc0c4'
+      const indexSelect = props.arraySelectedNumbers.indexOf(num)
+      props.arraySelectedNumbers.splice(indexSelect, 1)
+    } else {
+      alert(`A ${props.selectedGame.type} pode ter até ${props.selectedGame.maxNumber} números selecionados`)
+    }
+  }
+
+  return (
+    <ButtonNumberContainer
+      selected={props.selected}
+      onPress={(e) => handleSelectNumber(e)}
+      data-js="button-number"
+    >
+      <NumberText>{String(props.value).padStart(2, '0')}</NumberText>
+    </ButtonNumberContainer>
+  )
+
+}
+
+export default ButtonNumber
---
+++ src/components/ButtonNumber/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native'
+
+
+interface IButtonNumberContainerProps {
+  selected: boolean
+}
+
+export const ButtonNumberContainer = styled.Pressable<IButtonNumberContainerProps>`
+  background-color: ${props => props.selected ? '#27c383' : '#adc0c4'};
+  border: none;
+  border-radius: 50%;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  width: 59px;
+  height: 59px;
+  margin: 8px;
+`
+
+export const NumberText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+`
